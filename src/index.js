019b9a11b03626ba77c0e5192694ee4dd3ce30f7
Anthony Jung <junganthonyy@gmail.com>
2026-02-02 09:58:26 -0800

    Conditional Formatting

--- src/index.js
+++ src/index.js
@@ -58,8 +58,8 @@ function App() {
 }
 
 function Header() {
-  // const style = {};
-  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
 
   return (
     <header className="header">
@@ -69,19 +69,23 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {pizzaData.map((details) => (
-        <Pizza {...details} key={details.name} />
-      ))}
+      <ul className="pizzas">
+        {numPizzas > 0 &&
+          pizzas.map((details) => <Pizza {...details} key={details.name} />)}
+      </ul>
     </main>
   );
 }
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 8;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour < closeHour;
 
@@ -89,21 +93,26 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're Currently Open!
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 }
 
 function Pizza({ name, ingredients, photoName, price }) {
   return (
-    <div className="pizza">
+    <li className="pizza">
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
         <span>{price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
