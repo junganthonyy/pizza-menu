36b453f1c21eeecba50a5e77e513b719903d5aaa
Anthony Jung <junganthonyy@gmail.com>
2026-02-02 10:35:11 -0800

    Adding Fragment

--- src/index.js
+++ src/index.js
@@ -76,12 +76,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((details) => (
-            <Pizza {...details} key={details.name} />
-          ))}
-        </ul>
+        <React.Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((details) => (
+              <Pizza {...details} key={details.name} />
+            ))}
+          </ul>
+        </React.Fragment>
       ) : (
         <p>We're still working on our menu. Please come back later.</p>
       )}
