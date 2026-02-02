71ccbd7fe0ee4db8ea7fff00eb9c22fa2c37f42f
Anthony Jung <junganthonyy@gmail.com>
2026-02-02 10:14:49 -0800

    Finishing

--- src/index.js
+++ src/index.js
@@ -95,21 +95,33 @@ function Footer() {
   const closeHour = 22;
   const isOpen = hour >= openHour && hour < closeHour;
 
-  console.log(isOpen);
-
   return (
     <footer className="footer">
-      {isOpen && (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+      {isOpen ? (
+        <Order closeHour={closeHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
 }
 
-function Pizza({ name, ingredients, photoName, price }) {
+function Order({ closeHour }) {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online!</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
+function Pizza({ name, ingredients, photoName, price, soldOut }) {
+  if (soldOut) {
+    return null;
+  }
+
   return (
     <li className="pizza">
       <img src={photoName} alt={name} />
